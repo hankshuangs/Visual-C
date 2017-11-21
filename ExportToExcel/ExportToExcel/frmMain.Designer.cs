346168a008ef0f9f59ca00f4e43c693fcfb575d5
Hanks Huang <hankshuangs@gmail.com>
2017-11-22 00:56:03 +0800

    DataSet To Excel Sheets

---
+++ ExportToExcel/ExportToExcel/frmMain.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace ExportToExcel
+{
+    partial class frmExcelExport
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Image = global::ExportToExcel.Properties.Resources.Excel;
+            this.button1.Location = new System.Drawing.Point(83, 45);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(231, 220);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.btnExport_Click);
+            // 
+            // frmExcelExport
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(404, 313);
+            this.Controls.Add(this.button1);
+            this.Name = "frmExcelExport";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Export To Excel";
+            this.Load += new System.EventHandler(this.frmExcelExport_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ ExportToExcel/ExportToExcel/frmMain.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OfficeExcel = Microsoft.Office.Interop.Excel;
+
+namespace ExportToExcel
+{
+    public partial class frmExcelExport : Form
+    {
+        public frmExcelExport()
+        {
+            InitializeComponent();
+        }
+
+        private void frmExcelExport_Load(object sender, EventArgs e)
+        {
+        }
+
+        private DataSet GetDataSet()
+        {
+            DataSet ds = new DataSet();
+            DataTable dt1 = new DataTable();
+            dt1.Columns.Add("S1 NO"); //Define Columns
+            dt1.Columns.Add("Novel Name");
+            dt1.Columns.Add("Author");
+            dt1.Columns.Add("Genres");
+            dt1.Columns.Add("Published Date");
+            dt1.Columns.Add("Price");
+            dt1.Columns.Add("Rating");
+
+            dt1.Rows.Add("1", "In Search of Lose Time", "Marcel Proust", "Literary modernism", "01-01-1913", "348", "4.3"); //Adding Rows
+            dt1.Rows.Add("2", "Ulysses", "James Joyce", "Modernism", "22-02-1922", "58", "3.7");
+            dt1.Rows.Add("3", "Moby Dick", "Herman Melville", "Adventure fiction", "18-10-1851", "131", "3.4");
+            dt1.Rows.Add("4", "Hamlet", "William Shakespeare", "Tragedy", "01-01-1603", "225", "3.9");
+            dt1.Rows.Add("5", "War and Peace", "Leo Tolstoy", "Historical fiction", "01-01-1869", "133.95", "4.1");
+            dt1.TableName = "Table1";
+            ds.Tables.Add(dt1);
+
+            DataTable dt2 = dt1.Copy();
+            dt2.TableName = "Table2";
+            ds.Tables.Add(dt2);
+            DataTable dt3 = dt1.Copy();
+            dt3.TableName = "Table3";
+            ds.Tables.Add(dt3);
+
+            return ds;
+        }
+
+        private void btnExport_Click(object sender, EventArgs e)
+        {
+            DataSet dsData = GetDataSet();
+            ExportDataSetToExcel(dsData, Application.StartupPath);
+        }
+
+        private void ExportDataSetToExcel(DataSet ds, string strPath)
+        {
+            int inHeaderLength = 3, inColumn = 0, inRow = 0;
+            System.Reflection.Missing Default = System.Reflection.Missing.Value;
+            //Create Excel File
+            strPath += @"\Excel" + DateTime.Now.ToString("yyyyMMdd-hhmmss").Replace('/', '-') + ".xlsx";
+            OfficeExcel.Application excelApp = new OfficeExcel.Application();
+            OfficeExcel.Workbook excelWorkBook = excelApp.Workbooks.Add(1);
+            foreach (DataTable dt in ds.Tables)
+            {
+                //Create Excel WorkSheet
+                OfficeExcel.Worksheet excelWorkSheet = excelWorkBook.Sheets.Add(Default, excelWorkBook.Sheets[excelWorkBook.Sheets.Count]);
+                excelWorkSheet.Name = dt.TableName; //Name worksheet
+
+                //Write Column Name
+                for (int i = 0; i < dt.Columns.Count; i++)
+                    excelWorkSheet.Cells[inHeaderLength + 1, i + 1] = dt.Columns[i].ColumnName.ToUpper();
+                //Write Rows
+                for (int m = 0; m < dt.Rows.Count; m++)
+                {
+                    for (int n = 0; n < dt.Columns.Count; n++)
+                    {
+                        inColumn = n + 1;
+                        inRow = inHeaderLength + 2 + m;
+                        excelWorkSheet.Cells[inRow, inColumn] = dt.Rows[m].ItemArray[n].ToString();
+                        if (m % 2 == 0)
+                            excelWorkSheet.get_Range("A" + inRow.ToString(), "G" + inRow.ToString()).Interior.Color = System.Drawing.ColorTranslator.FromHtml("#FCE4D6");
+                    }
+                }
+
+                //Excel Header
+                OfficeExcel.Range cellRang = excelWorkSheet.get_Range("A1", "G3");
+                cellRang.Merge(false);
+                cellRang.Interior.Color = System.Drawing.Color.White;
+                cellRang.Font.Color = System.Drawing.Color.Gray;
+                cellRang.HorizontalAlignment = OfficeExcel.XlHAlign.xlHAlignCenter;
+                cellRang.VerticalAlignment = OfficeExcel.XlHAlign.xlHAlignCenter;
+                cellRang.Font.Size = 26;
+                excelWorkSheet.Cells[1, 1] = "Greate Novels Of All Time";
+
+                //Style table column names
+                cellRang = excelWorkSheet.get_Range("A4", "G4");
+                cellRang.Font.Bold = true;
+                cellRang.Font.Color = System.Drawing.ColorTranslator.ToOle(System.Drawing.Color.White);
+                cellRang.Interior.Color = System.Drawing.ColorTranslator.FromHtml("#ED7D31");
+                excelWorkSheet.get_Range("F4").EntireColumn.HorizontalAlignment = OfficeExcel.XlHAlign.xlHAlignRight;
+                //Formate prince column
+                excelWorkSheet.get_Range("F5").EntireColumn.NumberFormat = "0.00";
+                //Auto fit columns
+                excelWorkSheet.Columns.AutoFit();
+            }
+
+            //Delete First Page
+            excelApp.DisplayAlerts = false;
+            Microsoft.Office.Interop.Excel.Worksheet lastWorkSeet = (Microsoft.Office.Interop.Excel.Worksheet)excelWorkBook.Worksheets[1];
+            lastWorkSeet.Delete();
+            excelApp.DisplayAlerts = true;
+
+            //Set Defualt Page
+            (excelWorkBook.Sheets[1] as OfficeExcel.Worksheet).Activate();
+
+            excelWorkBook.SaveAs(strPath, Default, Default, Default, false, Default, OfficeExcel.XlSaveAsAccessMode.xlNoChange, Default, Default, Default, Default, Default);
+            excelWorkBook.Close();
+            excelApp.Quit();
+
+            MessageBox.Show("Excel generated successfully \n As " + strPath);
+        }
+    }
+}
